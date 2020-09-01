4d2400c999cbd88d6ac58c1ce05ade05965799cd
Petr D. Novak <petr-novak-donald@protonmail.com>
2020-09-01 16:10:41 +0200

    section 312 done.

---
+++ controllers/profile.js
@@ -0,0 +1,19 @@
+const handleProfile = (req, res) => (db) => {
+
+    const { id } = req.params;
+    db.select('*').from('users').where({
+        id
+    })
+    .then(user => {
+        if (user.length) {
+        res.json(user[0])
+        }
+        else {
+            res.status(400).json('This kind of Id is not in DB.')
+        }
+        })
+    .catch(err => res.status(400).json('error getting user'))
+
+}
+
+module.exports = { handleProfile };
