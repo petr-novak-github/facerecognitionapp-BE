7e07a82528af0d475a8b2b97d645538520f74590
Petr D. Novak <petr-novak-donald@protonmail.com>
2020-09-10 10:56:34 +0200

    change response

--- server.js
+++ server.js
@@ -27,40 +27,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    
-        
-    login:  
-    [
-        {
-    id: '987',
-    has: '',
-    email: 'john@gmail.com'
-    }    
-]
-    
-};
-
-
-app.get('/', (req, res) => {res.send(database.users)});
+app.get('/', (req, res) => {res.send('its working !!')});
 
 app.post('/signin', (req, res) =>  { signin.handleSignIn (req, res, db, bcrypt) });
 app.post('/register', (req, res) =>  { register.handleRegister (req, res, db, bcrypt) });
