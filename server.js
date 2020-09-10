e7e298dace5cb8e016c8a620e151cc5e91c339cd
Petr D. Novak <petr-novak-donald@protonmail.com>
2020-09-10 13:13:25 +0200

    pridana picovina od tsl z section 318 poznamky

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const signin = require('./controllers/signin');
 const profile = require ('./controllers/profile');
 const image = require ('./controllers/image');
 
-
+process.env.NODE_TLS_REJECTED_UNAUTHORIZED = 0
 
 const db = knex ({
    client: 'pg',
